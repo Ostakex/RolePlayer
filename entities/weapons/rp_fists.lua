825bee9a394707a300a0c779743de0e7bc174f39
Ingi Þór <ingitho@gmail.com>
2014-03-08 20:52:47 +0000

    rp_fists
    Bætti við basic fists, óklárað

---
+++ entities/weapons/rp_fists.lua
@@ -0,0 +1,41 @@
+AddCSLuaFile()
+
+SWEP.PrintName = "Fists"
+SWEP.Author = "CondoM"
+SWEP.Instructions = "Click to hnef"
+SWEP.Purpose = "The action of hnefing"
+
+SWEP.Slot = 2
+SWEP.SlotPos = 1
+SWEP.DrawAmmo = false
+SWEP.DrawCrosshair = true
+SWEP.HoldType = "fist"
+
+
+SWEP.ViewModelFOV = 70
+
+SWEP.Spawnable = true
+SWEP.Drop = false
+SWEP.AdminOnly = false
+
+SWEP.Primary.Recoil = 3
+SWEP.Primary.Damage = 30
+SWEP.Primary.NumShots = 1
+SWEP.Primary.Cone = 0
+SWEP.Primary.Delay = 0.5
+SWEP.Primary.ClipSize = -1
+SWEP.Primary.DefaultClip = -1
+SWEP.Primary.Ammo = ""
+SWEP.Primary.Automatic = false
+
+SWEP.Secondary.Clipsize = -1
+SWEP.Secondary.DefaultClip = -1
+SWEP.Secondary.Automatic = false
+SWEP.Secondary.Ammo = ""
+
+SWEP.ViewModel = "models/weapons/v_fists.mdl"
+SWEP.WorldModel = ""
+
+/* if (file.Exists("", "GAME")) then
+SWEP.WepSelectIcon = surface.GetTextureID("")
+end */
